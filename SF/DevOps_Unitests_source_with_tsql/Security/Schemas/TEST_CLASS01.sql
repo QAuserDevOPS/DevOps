ffd48a4f7172f8690dffa5979a9ebf0875f8e66a
QAuserDevOPS <60710363+QAuserDevOPS@users.noreply.github.com>
2020-02-05 19:37:49 +0100

    Add files via upload

---
+++ SF/DevOps_Unitests_source_with_tsql/Security/Schemas/TEST_CLASS01.sql
@@ -0,0 +1,5 @@
+﻿CREATE SCHEMA [TEST_CLASS01] AUTHORIZATION [dbo]
+GO
+
+EXEC sys.sp_addextendedproperty N'tSQLt.TestClass', 1, 'SCHEMA', N'TEST_CLASS01'
+GO
---
+++ SF/DevOps_Unitests_source_with_tsql/Security/Schemas/TEST_CLASSO4.sql
@@ -0,0 +1,5 @@
+﻿CREATE SCHEMA [TEST_CLASS04] AUTHORIZATION [dbo]
+GO
+
+EXEC sys.sp_addextendedproperty N'tSQLt.TestClass', 1, 'SCHEMA', N'TEST_CLASS04'
+GO
