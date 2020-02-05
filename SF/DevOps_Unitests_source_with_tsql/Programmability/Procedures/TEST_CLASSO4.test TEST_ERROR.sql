f57aec59284f79eb22355b0b133345e991ef4a92
QAuserDevOPS <60710363+QAuserDevOPS@users.noreply.github.com>
2020-02-05 19:50:12 +0100

    Add files via upload

---
+++ SF/DevOps_Unitests_source_with_tsql/Programmability/Procedures/TEST_CLASSO4.test TEST_ERROR.sql
@@ -0,0 +1,11 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+
+--  Comments here are associated with the test.
+--  For test case examples, see: http://tsqlt.org/user-guide/tsqlt-tutorial/
+CREATE PROCEDURE [TEST_CLASS04].[test TEST_ERROR]
+AS
+BEGIN
+ SELECT * FROM unnexist_table
+END;
+GO
